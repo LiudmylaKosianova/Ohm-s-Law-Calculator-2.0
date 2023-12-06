52bc92673bce293f53e19907162428ce7a910d07
snowwhitesheep <snw.wht.lk@gmail.com>
2023-12-06 15:09:53 +0100

    case 1 and 2

--- OhmsLawCalculator2.c
+++ OhmsLawCalculator2.c
@@ -7,18 +7,55 @@ double calc_V_from_PnR(double P, double R);//function to perform V=sqrt(P*R)
 double calc_V_from_InR(double I, double R);//function to perform V=P/I
 
 int main(){
-    print_menu();
+    int q = 0;
+    int c;
+    double P,R,I,V;
+
+    printf("Welcome to the Ohm's Law Calculator v2.0!");
+
+    do{
+        print_menu();
+        scanf(" %d", &c);
+        switch(c){
+            case 1:
+            printf("\n\tEnter P in Watts: ");
+            scanf(" %lf", &P);
+            printf("\tEnter I in Amps: ");
+            scanf(" %lf", &I);
+            V = calc_V_from_PnI(P,I);
+            printf("\n\tYour Voltage = %.3lf Volts", V);
+            case 2:
+            printf("\n\tEnter P in Watts: ");
+            scanf(" %lf", &P);
+            printf("\tEnter R in Ohms: ");
+            scanf(" %lf", &R);
+            V = calc_V_from_PnR(P,R);
+            printf("\n\tYour Voltage = %.3lf Volts", V);
+
+        }
+
+    }while(q != 4);
+    
 
     return 0;
 }
 
 void print_menu(void){
-    printf("Welcome to the Ohm's Law Calculator v2.0!\n\n");
+    printf("\n\n");
     printf("\tHere are your calculation options:\n\n");
     printf("\t1. Find Voltage based on Power and Current (V=P/I).\n");
     printf("\t2. Find Voltage based on Power and Resistance (V=sqrt(P*R)).\n");
     printf("\t3. Find Voltage based on Current and Resistance (V=I*R).\n");
     printf("\t4. Quit.\n\n");
     printf("Enter your choice: ");
+}
+double calc_V_from_PnI(double P, double I){
+    double V = P / I;
+    return V;
+}
+
+double calc_V_from_PnR(double P, double R){
+    double V = sqrt(P*R);
+    return V;
 }
 
